b3d1a78c47d27eb0ee567f1b5aa792ab050c9ece
blm34 <48328783+blm34@users.noreply.github.com>
2026-03-09 23:07:29 +0000

    Improve comments in remap.lua

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -2,7 +2,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- Move selected text with J and K in visual mode
+-- Move selected text up and down with J and K in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
 
@@ -20,17 +20,21 @@ vim.keymap.set("n", "N", function()
     require("mini.animate").execute_after("scroll", vim.cmd("normal! zzzv"))
 end)
 
--- Custom copy paste remaps
+-- <leader>p to paste from system clipboard
 vim.keymap.set("n", "<leader>p", "\"+p")
 vim.keymap.set("x", "<leader>p", "\"+p")
 
+-- <leader>y to copy to system clipboard
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 
--- Quick fix list
+-- <C-j> to go to next item in qfl
 vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
+-- <C-k> to go to previous item in qfl
 vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
+-- <leader>j to go to next item in location list
 vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz")
+-- <leader>k to go to previous item in location list
 vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz")
 
 -- Move between windows with Alt+direction
@@ -45,14 +49,15 @@ vim.keymap.set("n", "<CM-l>", function() vim.cmd("vertical resize +1") end)
 vim.keymap.set("n", "<CM-j>", function() vim.cmd("resize -1") end)
 vim.keymap.set("n", "<CM-k>", function() vim.cmd("resize +1") end)
 
--- Terminal commands
+-- Exit terminal mode with <ESC>
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
+-- Move between windows with Alt+direction in terminal mode
 vim.keymap.set("t", "<M-h>", "<C-\\><C-n><C-w>h")
 vim.keymap.set("t", "<M-j>", "<C-\\><C-n><C-w>j")
 vim.keymap.set("t", "<M-k>", "<C-\\><C-n><C-w>k")
 vim.keymap.set("t", "<M-l>", "<C-\\><C-n><C-w>l")
 
 -- add blank lines in normal mode
-vim.keymap.set("n", "go", "o<ESC>k")
-vim.keymap.set("n", "gO", "O<ESC>j")
+vim.keymap.set("n", "go", "o<ESC>")
+vim.keymap.set("n", "gO", "O<ESC>")
