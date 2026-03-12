6e2301d398d6f0a195f55d2f64e77665089b12c8
blm34 <48328783+blm34@users.noreply.github.com>
2026-03-12 22:17:18 +0000

    Add the flash plugin
    This makes the mini-jump plugin redundant so it is removed

---
+++ lua/plugins/workflow/flash.lua
@@ -0,0 +1,17 @@
+return {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {
+        search = {
+            exclude = { "NvimTree" },
+        },
+    },
+    keys = {
+        {
+            "<leader>/",
+            function() require("flash").jump() end,
+            mode = { "n", "x", "o" },
+            desc = "Flash",
+        },
+    }
+}
