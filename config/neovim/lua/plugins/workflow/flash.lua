93c069212d09c516c7040a78b3afa3caf91021c9
blm34 <48328783+blm34@users.noreply.github.com>
2026-04-30 23:08:37 +0100

    neovim: don't use flash keymap in qfl or command-line windows

--- config/neovim/lua/plugins/workflow/flash.lua
+++ config/neovim/lua/plugins/workflow/flash.lua
@@ -1,3 +1,19 @@
+-- Don't remap <Enter> in QFL window
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "qf" },
+    callback = function()
+        vim.keymap.set("n", "<CR>", "<CR>", { buffer = true, noremap = true })
+    end,
+})
+
+-- Don't remap <Enter> in command-line window (`q:`, `q/`, `q?`)
+vim.api.nvim_create_autocmd("CmdwinEnter", {
+    callback = function()
+        vim.keymap.set("n", "<CR>", "<CR>", { buffer = true, noremap = true })
+    end,
+})
+
+
 return {
     "folke/flash.nvim",
     event = "VeryLazy",
