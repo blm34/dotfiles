18bdba349f93ae3e367d7327aab1d43167f47900
blm34 <48328783+blm34@users.noreply.github.com>
2026-05-10 23:41:58 +0100

    neovim: make use of more features from the flash plugin

--- config/neovim/lua/plugins/workflow/flash.lua
+++ config/neovim/lua/plugins/workflow/flash.lua
@@ -1,19 +1,3 @@
--- Don't remap <Enter> in QFL window
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "qf" },
-    callback = function()
-        vim.keymap.set("n", "<CR>", "<CR>", { buffer = true, noremap = true })
-    end,
-})
-
--- Don't remap <Enter> in command-line window (`q:`, `q/`, `q?`)
-vim.api.nvim_create_autocmd("CmdwinEnter", {
-    callback = function()
-        vim.keymap.set("n", "<CR>", "<CR>", { buffer = true, noremap = true })
-    end,
-})
-
-
 return {
     "folke/flash.nvim",
     event = "VeryLazy",
@@ -21,13 +5,45 @@ return {
         search = {
             exclude = { "NvimTree" },
         },
+        modes = {
+            char = {
+                jump_labels = true,
+                config = function(opts)
+                    opts.autohide = opts.autohide or (vim.fn.mode(true):find("no") and vim.v.operator == "y")
+
+                    opts.jump_labels = opts.jump_labels
+                        and vim.fn.mode(true):find("no") == nil
+                        and vim.v.count == 0
+                        and vim.fn.reg_executing() == ""
+                        and vim.fn.reg_recording() == ""
+                end
+            },
+        },
     },
     keys = {
         {
-            "<CR>",
+            "s",
             function() require("flash").jump() end,
             mode = { "n", "x", "o" },
             desc = "Search with flash",
         },
-    }
+        {
+            "S",
+            function() require("flash").treesitter() end,
+            mode = { "n", "x", "o" },
+            desc = "Flash Treesitter",
+        },
+        {
+            "r",
+            function() require("flash").remote() end,
+            mode = "o",
+            desc = "Remote Flash",
+        },
+        {
+            "<c-s>",
+            function() require("flash").toggle() end,
+            mode = "c",
+            desc = "Toggle Flash Search",
+        }
+    },
 }
